1a383d3045a677bf2b0fb963e8ba7c07569d7bc9
subhampradhan <31339374+subhampradhan@users.noreply.github.com>
2017-09-29 07:56:56 +0530

    Create program7.cpp

---
+++ program7.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int rev(int, int);
+ 
+int main()
+{
+    int num, result;
+    int length = 0, temp;
+ 
+    cout<<"Enter an integer number to reverse: ";
+    cin>>num;
+    temp = num;
+    while (temp != 0)
+    {
+        length++;
+        temp = temp / 10;
+    }
+    result = rev(num, length);
+    cout<<"The reverse of "<<num<<"  is "<<result;
+    if(result==num)
+    {
+    	cout<<"  entered number is paliondrom";
+    }
+    else{
+    	cout<<"  entered number is not paliondrom";
+    }
+    return 0;
+}
+ 
+int rev(int num, int len)
+{
+    if (len == 1)
+    {
+        return num;
+    }
+    else
+    {
+        return (((num % 10) * pow(10, len - 1)) + rev(num / 10, --len));
+    }
+}
